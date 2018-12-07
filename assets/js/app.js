943c60f413f515f16f99b20d8c8b01473af2b53f
botflux <victor.mendele68@gmail.com>
2018-12-07 22:28:37 +0100

    feat: responsive menu

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+class MenuHandler {
+    constructor (config) {
+        this.menu = document.querySelector(config.menuSelector)
+        this.navLinks = document.querySelectorAll(config.navLinkSelector)
+        this.nav = document.querySelector(config.navSelector)
+        this.navActive = config.navActive
+        this.init()
+    }
+
+    init () {
+        this.menu.addEventListener('click', () => {
+            this.nav.classList.toggle(this.navActive)
+        })
+        this.navLinks.forEach(n => {
+            n.addEventListener('click', () => {
+                this.nav.classList.toggle(this.navActive)
+            })
+        })
+    }
+}
+
+new MenuHandler({
+    menuSelector: '[data-menu]',
+    navLinkSelector: '.nav-link',
+    navSelector: '[data-nav]',
+    navActive: 'nav--active'
+})
