09e497253dadf65a56f7613f55e849f7433cd801
botflux <victor.mendele68@gmail.com>
2018-12-08 23:25:31 +0100

    feat: loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,3 @@
-import './animation'
-
-class MenuHandler {
-    constructor (config) {
-        this.menu = document.querySelector(config.menuSelector)
-        this.navLinks = document.querySelectorAll(config.navLinkSelector)
-        this.nav = document.querySelector(config.navSelector)
-        this.navActive = config.navActive
-        this.init()
-    }
-
-    init () {
-        this.menu.addEventListener('click', () => {
-            this.nav.classList.toggle(this.navActive)
-        })
-        this.navLinks.forEach(n => {
-            n.addEventListener('click', () => {
-                this.nav.classList.toggle(this.navActive)
-            })
-        })
-    }
-}
-
-new MenuHandler({
-    menuSelector: '[data-menu]',
-    navLinkSelector: '.nav-link',
-    navSelector: '[data-nav]',
-    navActive: 'nav--active'
+import('./app-async').then(() => {
+    document.querySelector('.loading').style = `display: none;`
 })
-
