7f13b8730662002d6f2de359d119e2abb34ce60f
botflux <victor.mendele68@gmail.com>
2018-12-16 20:16:12 +0100

    feat: application settings interface

---
+++ src/Controller/Admin/AdminApplicationSettingsController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Form\ApplicationSettingsType;
+use App\Repository\ApplicationSettingsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminApplicationSettingsController extends AbstractController
+{
+    /**
+     * @var ApplicationSettingsRepository
+     */
+    private $repository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $manager;
+
+    /**
+     * AdminApplicationSettingsController constructor.
+     * @param ApplicationSettingsRepository $repository
+     * @param EntityManagerInterface $manager
+     */
+    public function __construct(ApplicationSettingsRepository $repository, EntityManagerInterface $manager)
+    {
+        $this->repository = $repository;
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route("/admin/application-settings", name="admin.application-settings.index", methods="GET")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function index (Request $request)
+    {
+        $settings = $this->repository->findSettings();
+        $form = $this->createForm(ApplicationSettingsType::class, $settings);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush();
+        }
+
+        return $this->render('admin/application-settings/index.html.twig', [
+            'settings' => $settings,
+            'form' => $form->createView()
+        ]);
+    }
+}
